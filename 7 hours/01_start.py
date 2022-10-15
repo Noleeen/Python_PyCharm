e72ca7647aae71b39c631292664894c856221161
Ivan Gerasimenko <noleen@ya.ru>
2022-10-15 12:48:46 +0300

    замена переменных без буфера, переменные из списка *, тип данных

---
+++ 7 hours/01_start.py
@@ -0,0 +1,25 @@
+# механизм обмена значений переменных без буферной переменной
+# swap1 = 8
+# swap2 = 9
+#
+# swap1, swap2 = swap2, swap1 + 10
+# swap2 **=2
+# print(swap2)
+
+
+# переменные із спіска
+
+# z, *x, c = [1, 2, 3, 4, 5]
+# print(z)
+# print(x)
+# print(c)
+
+# 1
+# [2, 3, 4]
+# 5
+
+
+# типы данных
+x, y = float(input()), float(input())
+r = x + y
+print('result' + str(r))
