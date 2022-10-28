3f93dafc1f33f4f4748a87c0874a5b8af097588b
Ivan Gerasimenko <noleen@ya.ru>
2022-10-28 23:48:08 +0300

    библиотека sympy для решения квадратного уравнения

--- Seminar4.py
+++ Seminar4.py
@@ -37,40 +37,73 @@
 
 # 2. Найдите корни квадратного уравнения Ax² + Bx + C = 0 ) с помощью математических формул нахождения корней квадратного уравнения
 
-a = 0
-b = 6
-c = 5
-d = discriminant = b ** 2 - 4 * a * c
+# # через sympy
+# import sympy as sm
+# a, b, c = map(int, input('enter a,b,c via space: ').split())
+# x = sm.Symbol('x')
+# print(sm.solveset(a*x**2 + b*x +c))
 
-if a == 0:
-    print(f'x = {-c / b}')
-else:
-    if d < 0:
-        print('no root of the equation')
-    else:
-        x1 = (-b + d ** 0.5) / 2 * a
-        x2 = (-b - d ** 0.5) / 2 * a
-        print('x1 = ', x1, ', x2 = ', x2)
+#АЛГОРІТМОМ
+# a = 0
+# b = 6
+# c = 5
+# d = discriminant = b ** 2 - 4 * a * c
+#
+# if a == 0:
+#     print(f'x = {-c / b}')
+# else:
+#     if d < 0:
+#         print('no root of the equation')
+#     else:
+#         x1 = (-b + d ** 0.5) / 2 * a
+#         x2 = (-b - d ** 0.5) / 2 * a
+#         print('x1 = ', x1, ', x2 = ', x2)
 
 # 3. Задайте два числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
+#
+# # ___________________________________________
+# # решение через НОК
+# a, b = map(int, input('enter number via space: ').split())
+# for i in range (min(a,b), a * b + 1, min(a,b)):
+#     if i % a == 0 and i % b == 0:
+#         print(f'nok = {i}')
+#         break
+# # ____________________________________________
+
+# второе решеніе через НОД
+
 # def chek_number(t):
 #     check = False
 #     while not check:
 #         try:
-#             num = float(input(t))
+#             num = int(input(t))
 #             check = True
 #         except ValueError as er:
 #             print(f'enter float number - {er}')
 #     return num
 # a = chek_number('enter number: ')
 # b = chek_number('enter number: ')
-# l = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-# print(type(a))
 #
-# for i in l:
+# for i in range(min(a,b), 0, -1):
 #     if a % i == 0 and b % i == 0:
 #         nod = i
 #         break
 #
 # nok = a * b / nod
 # print(nok)
+
+#------------------------------------------------------------------
+# задайте строку из набора чисел. напишите программу, которая покажет большее и меньшее число.
+# В качестве символа-разделителя исп пробел.
+
+# n = list(map(int, input('enter numbers: ').split(' ')))
+# minn = min(n)
+# maxx = max(n)
+#
+# # n = list(string)
+# print(f'{n}\nmin = {min(n)}\nmax = {max(n)}')
+
+
+
+
+
