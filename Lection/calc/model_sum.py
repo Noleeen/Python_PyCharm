ad3cc44b38f2ee4776d49fb86b37ca11154782d5
Ivan Gerasimenko <noleen@ya.ru>
2022-11-15 15:26:55 +0300

    calc start2

---
+++ Lection/calc/model_sum.py
@@ -0,0 +1,11 @@
+x = 0
+y = 0
+
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+def sum():
+    return x + y
---
+++ Lection/calc/view.py
@@ -0,0 +1,5 @@
+def view_data(data):
+    print(f'resalt = {data}')
+
+def get_value():
+    return int(input('value = '))
