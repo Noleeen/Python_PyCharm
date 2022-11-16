8f2dc8ec1768ba8a9dc615533c3834fe8433c438
Ivan Gerasimenko <noleen@ya.ru>
2022-11-16 15:38:20 +0300

    calc-mult

--- Lection/calc/controller.py
+++ Lection/calc/controller.py
@@ -1,9 +1,9 @@
-import model_sum as model
+import model_mult as model
 import view
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
     model.init(value_a, value_b)
-    result = model.sum()
+    result = model.mult()
     view.view_data(result)
