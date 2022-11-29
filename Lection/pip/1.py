2e80aad42ad5423d49addb97295fa48ac626fd55
Ivan Gerasimenko <noleen@ya.ru>
2022-11-29 11:50:27 +0300

    1

---
+++ Lection/pip/1.py
@@ -0,0 +1,12 @@
+# from isOdd import isOdd
+#
+# print(isOdd(1))
+# print(isOdd(5))
+#
+# print(isOdd(0))
+# print(isOdd(4))
+
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
+print(emoji.emojize("Python is fun :red_heart:"))
