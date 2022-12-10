afb3697790973995e7dc8a6a257f890fa22f9b37
Ivan Gerasimenko <noleen@ya.ru>
2022-12-10 23:25:33 +0300

    bot

--- HomeWork/ hw_9_TBot_calc_krestiki-noliki/bot.py
+++ HomeWork/ hw_9_TBot_calc_krestiki-noliki/bot.py
@@ -10,6 +10,20 @@ kb_main.add('/calc').insert('/X_and_0')
 
 kb_xo = ReplyKeyboardMarkup(resize_keyboard= True)
 kb_xo.add('/play_with_bot').insert('/2_players')
+
+kb_xo_play = ReplyKeyboardMarkup(resize_keyboard= True)
+kb_xo_play.add('/0').insert('/1').insert('/2').add('/3').insert('/4').insert('/5').add('/6').insert('/7').insert('/8')
+
+
+game_list = ['-'] * 9
+
+def list_print(l):
+    l = f'''
+{game_list[0]}  {game_list[1]}  {game_list[2]}
+{game_list[3]}  {game_list[4]}  {game_list[5]}
+{game_list[6]}  {game_list[7]}  {game_list[8]}
+    '''
+    return l
 async def on(_):
     print('bot is runing')
 
@@ -29,7 +43,9 @@ async def com_2_players(message: types.Message):
 @dp.message_handler(commands=['play_with_bot'])
 async def com_play_with_bot(message: types.Message):
     await message.answer('ok, let\'s go!')
-
+    await message.answer(list_print(game_list), reply_markup=kb_xo_play)
+    game_list[8] = 'X'
+    await message.answer(list_print(game_list), reply_markup=kb_xo_play)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on, skip_updates= True)
