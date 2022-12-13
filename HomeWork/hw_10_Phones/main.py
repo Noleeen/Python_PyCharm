ecc963133f4553bee0e8f48e72d380b3d55256e7
Ivan Gerasimenko <noleen@ya.ru>
2022-12-13 18:11:41 +0300

    hw 10 phones

---
+++ HomeWork/hw_10_Phones/main.py
@@ -0,0 +1,13 @@
+phones = {'Силик':
+              {'phones': ['123','54567'],
+               'adress' : 'Минск',
+               'birthday' : '01.05.1987',
+               'e-mail' : 'sdf@tut.by'},
+          'Паша':{'phones': ['126','554667'],
+               'adress' : 'Минск',
+               'birthday' : '13.05.1986',
+               'e-mail' : 'sd@tut.by'}}
+
+print(phones['Силик'])
+print(phones['Силик']['adress'])
+print(len(phones['Паша']['phones']))
